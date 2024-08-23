75c4c77e96d4a09deadbd1161f70e81b6cd43cbb
akashh <akashyoungstar123@gmail.com>
2024-08-23 20:20:58 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -104,6 +104,16 @@ function App() {
       setIsLoading(false);
       return;
     }
+     if (userInput.trim().toLowerCase() === 'navya') {
+      setChatHistory(prevHistory => [
+        ...prevHistory,
+        { role: 'user', text: userInput },
+        { role: 'model', text: 'navya poe kaliyanum kazhikk samad.ai parayn prj' }
+      ]);
+      setUserInput('');
+      setIsLoading(false);
+      return;
+    }
 
     const genAI = new GoogleGenerativeAI(API_KEY);
     const model = genAI.getGenerativeModel({ model: MODEL_NAME });
